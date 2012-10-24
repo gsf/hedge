64a464d3d0894c03a0eff486645b106086cc0b0b
Gabriel Farrell <g@grrawr.com>
2012-10-24 03:48:41 -0400

    Now with validate option

--- hedge.js
+++ hedge.js
@@ -1,21 +1,26 @@
-var crypto = require('crypto')
+var parser = require('basic-auth-parser')
 
 
 module.exports = function (opts) {
-  // TODO handle encrypted passwords
-  //var encrypt = function (str) {return str}
-  //if (opts.encryption) {
-  //  encrypt = crypto.createHash(opts.encryption)
-  //}
-  return function hedge (req, res, next) {
-    var base64str, pair
-    if (req.headers.authorization &&
-        req.headers.authorization.substr(0, 5) == 'Basic') {
-      base64str = req.headers.authorization.split(' ')[1]
-      pair = Buffer(base64str, 'base64').toString().split(':')
-      if (opts.pairs[pair[0]] === pair[1]) return next()
-    }
+  var realm = opts.realm || 'luxembourg'
+  var validate = opts.validate || function () {}
+
+  var needAuth = function (res) {
     res.writeHead(401, {'WWW-Authenticate': 'Basic realm="'+opts.realm+'"'})
     res.end()
+  }
+
+  return function hedge (req, res, next) {
+    var parsed
+    if (req.headers.authorization) {
+      parsed = parser(req.headers.authorization)
+      if (parsed.scheme == 'Basic') {
+        return validate(parsed.username, parsed.password, function (err) {
+          if (err) return needAuth(res)
+          next()
+        })
+      }
+    }
+    needAuth(res)
   }
 }
